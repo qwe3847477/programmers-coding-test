97a26752a17589af7dac6d28cfc76e3d9fa02473
조민준 <86594983+qwe3847477@users.noreply.github.com>
2022-05-30 19:02:14 +0900

    Create 스킬트리.cs

---
+++ Level 2/Summer Winter Coding(~2018)/스킬트리.cs
@@ -0,0 +1,24 @@
+public class Solution
+{
+    public int solution(string skill, string[] skill_trees)
+    {
+        int answer = 0;
+
+        foreach (string _skills in skill_trees)
+        {
+            int i = 0;
+
+            foreach (char _skill in _skills)
+            {
+                if (skill.Contains(_skill) && skill[i] == _skill)
+                    i++;
+                else if (skill.Contains(_skill) && skill[i] != _skill)
+                    goto Exit;
+            }
+
+            answer++;
+            Exit:;         
+        }
+        return answer;
+    }
+}
